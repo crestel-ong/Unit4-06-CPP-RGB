bb9271a17a83e28f0382e194f96fa71044ae1ca6
crestel-ong <crestel.ong@mths.ca>
2021-10-07 01:16:32 +0000

    good?

--- RGB.cpp
+++ RGB.cpp
@@ -8,13 +8,11 @@
 
 int main() {
     // this function prints out all the RGB possible combinations
-
-
     for (int counter1 = 0; counter1 < 256; counter1++) {
         for (int counter2 = 0; counter2 < 256; counter2++) {
             for (int counter3 = 0; counter3 < 256; counter3++) {
-                std::cout << counter1 << "," << counter2 << "," << counter3
-                << std::fixed << std::endl;
+                std::cout << "RGB (" << counter1 << ", " << counter2
+                << ", " << counter3 << ")" << std::endl;
             }
         }
     }
